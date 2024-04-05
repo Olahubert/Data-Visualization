1b68d405e391c87cf54c5d05aeaf819faf5a5c9d
hubert omo <visitolawaleomoyeni@gmail.com>
2024-04-04 20:05:42 -0400

    correction

--- piechart2.py
+++ piechart2.py
@@ -8,7 +8,8 @@ import numpy as np
 items = ["Rent", "Groceries", "Utilities", "Transportation", "Entertainment", "Savings"]
 data = [35, 20, 15, 10, 10, 10]  # Percentages can be adjusted
 plt.pie(data,labels=items)
-plt.pie(data, labels=items, autopct="%.2f%%")
+colors = ['blue', 'green', 'orange', 'red', 'yellow', 'purple']
+plt.pie(data, labels=items,colors=colors, autopct="%.2f%%")
 plt.title("Monthly_Expenses")
 # plt.show()
 plt.savefig("generatedImage.png")
